9ef5745c3e9147e3ac0ffcaa1b3ee744859c3318
Niemand <yurii.akkermanov@gmail.com>
2014-06-03 22:10:53 +1200

    a

--- brainfuck.py
+++ brainfuck.py
@@ -2,7 +2,7 @@ def brainfuck(b):
 	"""
 	Pybfi - Python Brainfuck interpreter
 	By Herr Niemand 2014 
-	Version 0.09
+	Version 0.10
 	Origin: https://github.com/herrniemand/python_brainfuck
 
 	arguments:
@@ -10,25 +10,26 @@ def brainfuck(b):
 
 	help: http://c2.com/cgi/wiki?BrainfuckLanguage
 
-	Copyrights NobodyAtAll. 
+	Copyrights Herr Niemand. 
 	All right reserved, preserved, precooked and prepared to rage agaings the machines!
+	GPLv2
 	"""
 	i = x = 0
 	s = buff = ''
-	arr = [0 for n in range(0,30000)]
+	arr = [0 for n in range(0,30000)]#30000 memory cells
 	l = len(b)
+	br = 0
 	while x < l:
 		n = b[x]
-		if s == '' or n == ']':
+		if (s == '' or n == ']') and not n.isalpha():
 			if n == '>': i += 1
-			if n == '<': i -= 1
-			if n == '+': arr[i] = (arr[i] + 1)%255
-			if n == '-': arr[i] = (arr[i] - 1)%255
-			if n == '.': buff += chr(arr[i])
-			if n == ',': pass
-			if n == '[': s = 'k'
-
-			if n == ']':
+			elif n == '<': i -= 1
+			elif n == '+': arr[i] = (arr[i] + 1)%255
+			elif n == '-': arr[i] = (arr[i] - 1)%255
+			elif n == '.': buff += chr(arr[i])
+			elif n == ',': s = 'c'
+			elif n == '[': s = 'w'
+			elif n == ']' and s == 'w':
 				while arr[i] > 0:
 					for z in s[0:]:
 						if z == '>': i += 1
@@ -37,8 +38,16 @@ def brainfuck(b):
 						if z == '-': arr[i] = (arr[i] - 1)%255
 						if z == '.': buff += chr(arr[i])
 				s = ''
-		else:
-			s += n
+			else: 
+				raise SyntaxError("Missing [.")
+
+		elif s == 'w': s += n
+		elif s == 'c': 
+			arr[i] = ord(n)
+			s = '' 
 
 		x += 1
+		if x == l and s != '': raise SyntaxError("Missing ].")
+
 	return buff
+print(brainfuck(',c'))
