9f8374bd6d24e37991c5fb44ea2c5ab09b9c6dae
Niemand <yurii.akkermanov@gmail.com>
2014-06-04 10:44:29 +1200

    Sampled added. Useless code removed.

--- Pybfir.py
+++ Pybfir.py
@@ -2,8 +2,9 @@ class Pybfir:
 	"""
 		Pybfir - Python Brainfuck interpreter Recursive
 		By Herr Niemand 2014 
-		Version 1.0.0
+		Version 1.1.0
 		Origin: https://github.com/herrniemand/python_brainfuck
+		Samples: https://github.com/pablojorge/brainfuck
 		help: http://c2.com/cgi/wiki?BrainfuckLanguage
 
 		Copyrights Herr Niemand. 
@@ -22,45 +23,41 @@ class Pybfir:
 
 			help: http://c2.com/cgi/wiki?BrainfuckLanguage
 		"""
-		try:
-			x = br = 0
-			s = ''
-			l = len(b)
-			while x < l:
-				n = b[x]
-				if not br or n in '][':
-					if n == '>': self.__i += 1
-					elif n == '<': self.__i -= 1
-					elif n == '+': self.__mem[self.__i] = (self.__mem[self.__i] + 1)%255
-					elif n == '-': self.__mem[self.__i] = (self.__mem[self.__i] - 1)%255
-					elif n == '.': print(chr(self.__mem[self.__i]))
-					elif n == ',':	
-						while True:
-							z = ''
-							try:
-								z = str(input("Please enter a character: "))
-								if ord(z) < 256: 
-									self.__mem[self.__i] = ord(z)
-									break
-								else: raise ValueError
-							except: print(z + ' is not a valid input!')
-						
-					elif n == '[': 
-						if br: s += n
-						br += 1
+		x = br = 0
+		s = ''
+		l = len(b)
+		while x < l:
+			n = b[x]
+			if not br or n in '][':
+				if n == '>': self.__i += 1
+				elif n == '<': self.__i -= 1
+				elif n == '+': self.__mem[self.__i] = (self.__mem[self.__i] + 1)%255
+				elif n == '-': self.__mem[self.__i] = (self.__mem[self.__i] - 1)%255
+				elif n == '.': print(chr(self.__mem[self.__i]), end='')
+				elif n == ',':	
+					while True:
+						z = ''
+						try:
+							z = str(input('Please enter a character: '))
+							if ord(z) < 256: 
+								self.__mem[self.__i] = ord(z)
+								break
+							else: raise ValueError
+						except: print(z + ' is not a valid input!')
+					
+				elif n == '[': 
+					if br: s += n
+					br += 1
 
-					elif n == ']' and br:
-						br -= 1
-						if not br:
-							while self.__mem[self.__i]: self.run(s)
-							s = ''
-						else: s += n
+				elif n == ']' and br:
+					br -= 1
+					if not br:
+						while self.__mem[self.__i]: self.run(s)
+						s = ''
+					else: s += n
 
-				elif br: s += n
-				x += 1
-
-		except Exception as e:
-			print(e.__class__.__name__,':',str(e).capitalize())
+			elif br: s += n
+			x += 1
 
 	def load(self, filename):
 		"""
